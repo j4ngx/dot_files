9c28e061ec8270b65de630b3889323dcf2693344
j4ngx <joseng2709@gmail.com>
2025-02-14 21:26:14 +0100

    feat(config): add ZSH customizations and Heapbytes theme, update installation script

--- config_files/full_config.sh
+++ config_files/full_config.sh
@@ -3,7 +3,7 @@
 sudo apt update && sudo apt upgrade -y
 
 # Install ZSH, lsd, and bat
-sudo apt install -y zsh lsd bat
+sudo apt install -y zsh lsd bat curl
 
 zsh --version
 
@@ -19,3 +19,22 @@ fi
 
 # Install OH-MY-ZSH
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+
+ZSH_CUSTOM=${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting
+git clone https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/sudo ${ZSH_CUSTOM}/plugins/sudo
+
+if [ -f ./zshrc ]; then
+    cp ./zshrc ~/.zshrc
+else
+    echo ".zshrc file not found in the current directory."
+fi
+
+git clone https://github.com/heapbytes/heapbytes-zsh $ZSH_CUSTOM/themes/heapbytes
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="heapbytes"/g' ~/.zshrc
+cp -r ./heapbytes $ZSH_CUSTOM/themes/heapbytes/heapbytes.zsh-theme
+
+mkdir -p $HOME/app-data
+
